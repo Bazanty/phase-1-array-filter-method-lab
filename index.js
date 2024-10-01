bac91b63108033e056c323bfcb116e392e4be2d7
bazanty <bazantyloffan@gmail.com>
2024-10-01 12:18:17 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+ function findMatching(drivers,driver){
+    function checkdrivers (dri){
+        if (dri.toLowerCase() == driver.toLowerCase()){
+            return dri
+        }
+    }
+    return drivers.filter(checkdrivers);
+    
+
+ }
+
+ function fuzzyMatch (drivers,letter){ 
+     function checkdrivers(driver){
+      if (driver.slice(0,letter.length) == letter) { 
+        return driver
+    }  
+    
+ }
+return drivers.filter(checkdrivers);
+}
+
+function matchName(drivers,driver){
+    function checkdrivers(obj){
+        for(let name in obj)
+            if(obj[name] === driver){
+                return obj
+            }
+        }
+ return drivers.filter(checkdrivers);
+    }
+
+   
